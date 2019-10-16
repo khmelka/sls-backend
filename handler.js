59e67759b1a03f0563484ad356007d4145871571
Nelly <nkhmeleva@yahoo.com>
2019-10-16 13:08:51 -0600

    init

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
 'use strict';
+const AWS = require("aws-sdk");
+const db = new AWS.DymanoDB.DocumentClient({ apiVersion: "2012-08-10"});
 
-module.exports.run = async (event) => {
+module.exports.run = async (event, context, callback) => {
   return{
     statusCode: 200,
     body: JSON.stringify({
